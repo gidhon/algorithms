95be9e25166329d4e1c259a37248d3429370c7d5
Gideon Kreitzer <ontwikkel@n86k.com>
2020-07-28 08:33:15 -0400

    + palindrome checker

---
+++ JavaScript/palindrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(string) {
+
+	string = string.toLowerCase();
+
+	const characters = string.split('');
+	const validChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+	let vettedChars = [];
+	characters.forEach(character => {
+
+		if (validChars.indexOf(character) > -1) vettedChars.push(character);
+
+	});
+
+	return vettedChars.join('') === vettedChars.reverse().join('') ? true : false;
+}
+
+isPalindrome('Eva, can I see bees in a cave?');
